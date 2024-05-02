cb1c1f80d412c396882d470f24611c2a654a1cac
LN <leonelngoya@gmail.com>
2024-05-02 08:17:31 +0100

    🖥️ wip: continue layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Main from "@/components/main";
+
 export default function Home() {
   return (
-    <main className="">
-      <h1 className="text-7xl mx-auto font-bold font-tartuffo">pour ceux qui osent être différents</h1>
+    <main className="relative">
+      <Header />
+      <Main />
+      <Footer />
     </main>
   );
 }
